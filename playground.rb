5adad68c934a413d0012ac1f283fb4f75de30267
Luis <lfmnovaes@gmail.com>
2021-12-13 19:42:37 -0300

    Create Class Student

--- playground.rb
+++ playground.rb
@@ -1,4 +1,5 @@
 require './person'
+require './student'
 
 person1 = Person.new(age:42, name:"John Doe", parent_permission:false)
 p person1
@@ -12,4 +13,6 @@ person3 = Person.new(age:16, parent_permission:true)
 p person3
 p person3.can_use_services?
 
-
+student1 = Student.new(classroom:"3A", age:18, name:"Jose", parent_permission:false)
+p student1
+p student1.play_hooky
