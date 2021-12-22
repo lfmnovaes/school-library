6fe294d32f6b78a83ac40508885c5f58b334b391
mike2611 <mpuentes2611@gmail.com>
2021-12-22 16:32:02 -0600

    Add objects to rentals array

--- test.rb
+++ test.rb
@@ -5,31 +5,11 @@ require './persistor'
 require './choice'
 require 'json'
 
-# people = []
-# people.push(Student.new(age: 14, name: 'Maria', parent_permission: false))
-# people.push(Student.new(age: 17, name: 'John', parent_permission: true))
-# books = []
-# books.push(Book.new('LOTR', 'Tolkien'))
-# books.push(Book.new('42', 'Lorem Ipsum'))
-# rentals = []
-# rentals.push(Rental.new('2021-12-03', books[0], people[0]))
-# rentals.push(Rental.new('2021-12-22', books[0], people[1]))
-# rentals.push(Rental.new('2021-12-22', books[1], people[1]))
-
-# # puts JSON.generate(rentals[0].to_json)
-
-# p = Persistor.new
-# p.save(people: people, books: books, rentals: rentals)
-
 people = HandlePerson.new
 people.read_people_json
-# puts people.people
-# #puts ''
 
 books = HandleBooks.new
 books.read_books_json
-# puts books.books
-#puts ''
 
 rentals = HandleRentals.new
 rentals.read_rentals_json(people.people, books.books)
